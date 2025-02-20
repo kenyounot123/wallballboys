ae555c0e2efcb180e941d43d092486e4cf772350
koreanpro22 <dhskim22@gmail.com>
2025-02-20 16:29:27 -0500

    finished supabase auth route with google provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,40 @@
-import Image from "next/image";
+import { getSession, signOut, signIn, getUsers } from "@/lib/actions";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getSession();
+  // const sessionData = session.json()
+  const user = session?.user;
+  // console.log("current session", user?.identities);
+  const users = await getUsers()
+
+  console.log(users)
   return (
-    <h1 className="text-4xl font-bold">Hello World</h1>
+    <div>
+      <h1 className="text-4xl font-bold">Hello World</h1>
+      {!user && (
+        <form action={signIn}>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+          >
+            Sign In
+          </button>
+        </form>
+      )}
+
+      {user && (
+        <div>
+          <p>user data: {user.email}</p>
+          <form action={signOut}>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded"
+            >
+              Sign Out
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
   );
 }
